496f6a70200ee2f6081cc22636eba11d92cf9ba4
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-06 09:49:34 +0300

    ft: comment resource - Created resource for comments table

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :comments
   resources :skill_sets
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
