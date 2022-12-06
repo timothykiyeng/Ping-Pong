bfc817e714ee08fe798d1e031de91bc2f27a2014
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-06 14:20:50 +0300

    ft: create route - added create route to post a comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,11 +8,20 @@ class CommentsController < ApplicationController
     render json: comment, status: :ok
   end
 
+  def create
+    comment = Comment.create!(comment_params)
+    render json: comment, status: :created
+  end
+
 
 
   private
 
   def find_comment
     Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.permit(:description, :skill_set_id, :user_id)
   end
 end
