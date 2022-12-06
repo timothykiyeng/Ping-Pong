da81746a22deb22edbbc7274c1fc6e7e90b6b678
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-06 14:45:47 +0300

    ft: exception handling - raise exception when a record is invalid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
 
   def render_unprocessable_entity(invalid)
-    render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+    render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
   end
 
 end
