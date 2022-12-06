72bd52da8d70369555203fa5324dbcf8017cb608
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-06 15:21:25 +0300

    ft: index/show routes - display all/single skillset(s)

--- app/controllers/skill_sets_controller.rb
+++ app/controllers/skill_sets_controller.rb
@@ -1,2 +1,19 @@
 class SkillSetsController < ApplicationController
+
+  def index
+    render json: SkillSet.all, status: :ok
+  end
+
+  def show
+    skillset = find_skill
+    render json: skillset, status: :ok
+  end
+
+
+
+  private
+
+  def find_skill
+    SkillSet.find(params[:id])
+  end
 end
