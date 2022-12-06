967bb6a40bde9c1912d1758435474441a018b2cd
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-06 15:33:29 +0300

    ft: update route - update a skill_set

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
   before_action :authorize
 
+
+  private 
   def authorize
     @current_user = User.find_by(id: session[:user_id])
     render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
--- app/controllers/skill_sets_controller.rb
+++ app/controllers/skill_sets_controller.rb
@@ -9,11 +9,21 @@ class SkillSetsController < ApplicationController
     render json: skillset, status: :ok
   end
 
+  def update
+    skillset = find_skill
+    skillset.update!(skill_params)
+    render json: skillset, status: :accepted
+  end
+
 
 
   private
 
   def find_skill
     SkillSet.find(params[:id])
+  end
+
+  def skill_params
+    params.permit(:is_favorite)
   end
 end
