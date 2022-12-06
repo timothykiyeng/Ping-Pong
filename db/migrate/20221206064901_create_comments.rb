5fe417f14a6be73d1cd2f8d57b9a9bb78c8f61de
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-06 10:03:38 +0300

    ft: foreign keys - added foreign keys to comments table

--- db/migrate/20221206064901_create_comments.rb
+++ db/migrate/20221206064901_create_comments.rb
@@ -1,6 +1,9 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
+      t.string :description
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :skill_set, null: false, foreign_key: true
 
       t.timestamps
     end
