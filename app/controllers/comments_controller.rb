7bea85cca0f80ab0e91eabc99af766eab659572c
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-06 14:18:14 +0300

    ft: show route - added show route to get one comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,18 @@
 class CommentsController < ApplicationController
   def index
     render json: Comment.all, status: :ok
-end
+  end
+
+  def show
+    comment = find_comment
+    render json: comment, status: :ok
+  end
+
+
+
+  private
+
+  def find_comment
+    Comment.find(params[:id])
+  end
 end
