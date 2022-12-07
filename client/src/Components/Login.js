85f08d07db2d9188190f5107ad27f52edda3bb6a
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-07 10:31:14 +0300

    ft: Login function - displays login page

---
+++ client/src/Components/Login.js
@@ -0,0 +1,10 @@
+import React, {useState} from "react";
+import LoginForm from "./LoginForm";
+import SignUpForm from "./SignUpForm";
+
+function Login( {onLogin} ){
+
+    const [showLogin, setShowLogin] = useState(true);
+
+
+}
