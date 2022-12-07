20ff2b39bc82942a484735a90895180f4d308a00
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-07 10:03:30 +0300

    ft: LoginForm - login form fields

--- client/src/Components/LoginForm.js
+++ client/src/Components/LoginForm.js
@@ -28,4 +28,52 @@ function LoginForm({ onLogin }) {
           });
       }
 
+
+      return (
+        <form onSubmit={handleSubmit}>
+        <FormField>
+            <Label htmlFor="username">Username</Label>
+            <Input
+              type="text"
+              id="username"
+              autoComplete="off"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </FormField>
+          <FormField>
+            <Label htmlFor="password">Password</Label>
+            <Input
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </FormField>
+
+          <FormField>
+            <Label htmlFor="confirm-password">Confirm Password</Label>
+            <Input
+              type="confirm-password"
+              id="confirm-password"
+              autoComplete="current-password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </FormField>
+
+            <Button variant="fill" color="primary" type="submit">
+              {isLoading ? "Loading..." : "Login"}
+            </Button>
+
+          <FormField>
+            {errors.map((err) => (
+              <Error key={err}>{err}</Error>
+            ))}
+          </FormField>
+        </form>
+      );
+
 }
+export default LoginForm;
