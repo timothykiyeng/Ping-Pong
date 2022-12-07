2f3a9ba5b8be3b041f53f04235744ae65ab2462d
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-07 09:55:32 +0300

    ft: handleSubmit - handles submission on FormSubmit

--- client/src/Components/LoginForm.js
+++ client/src/Components/LoginForm.js
@@ -1,11 +1,31 @@
 import React, {useState} from "react";
 
-function LoginForm() {
+function LoginForm({ onLogin }) {
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("")
     const [errors, setErrors] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setIsLoading(true);
+        fetch("/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, password, confirmPassword }),
+        })
+          .then((r) => {
+            setIsLoading(false);
+          if (r.ok) {
+            r.json().then((user) => onLogin(user));
+          } else {
+            r.json().then((err) => setErrors(err.errors));
+          }
+          });
+      }
 
 }
