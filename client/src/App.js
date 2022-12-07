0f9ffff644f29a40b094f834935749e3a7dc60d4
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-07 14:20:25 +0300

    ft: useEffect(me) - fetch all users

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
 
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import { Route, Routes } from "react-router-dom";
+import Header from './Components/Header';
+import Login from './Components/Login'
 
 function App() {
+
+  const [user, setUser] = useState(null);
+  const [skillSet, setSkillSet] = useState([]);
+  //const [videoData, setVideoData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/me").then((response) => {
+      if (response.ok) {
+        response.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+
   return (
     <h1>Let's Build this</h1>
   );
