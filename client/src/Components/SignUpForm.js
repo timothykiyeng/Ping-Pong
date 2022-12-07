eadd3fbc66fead4fcb1cf8420f0738260e15dee2
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-07 10:07:20 +0300

    ft: SignUpForm component - handle users signup fields

---
+++ client/src/Components/SignUpForm.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+
+function SignUpForm(){
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("")
+    const [errors, setErrors] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+
+}
