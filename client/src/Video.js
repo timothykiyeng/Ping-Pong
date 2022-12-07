728c18d7ecf011fb9b370f3200c9767cc3f96d13
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-07 16:59:17 +0300

    ft: Video function - displays the related video

---
+++ client/src/Video.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./styles";
+import { Link } from 'react-router-dom';
+import YoutubeEmbed from "./YoutubeEmbed";
+
+export default function Video({ videoData }) {
+
+  const {title, category, description, video } = videoData
+
+  return (
+    <div className="video-container">
+      <Link exact to="*" className="literally-back-button">Back</Link>
+      <YoutubeEmbed videoData={video} />
+      <h2 className="video-title" >{title}</h2>
+      <h5 className="video-category">{category}</h5>
+      <p className="video-description">{description}</p>
+    </div>
+  );
+}
