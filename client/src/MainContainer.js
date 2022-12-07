f5b3c82c4028f00f204ce963a600de892680a979
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-07 16:52:40 +0300

    ft: Props for MainContainer - receive props passed down

---
+++ client/src/MainContainer.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import CommentList from "./CommentList";
+import ShowComment from "./ShowComment"
+import { Route, Routes } from "react-router-dom";
+import Video from './Video';
+import SkillSetList from './SkillSetList'
+
+
+export default function MainContainer({ yogaData, handleUpdateItem, handleFindVideoData, videoData, user }) {
+
+    const [clicked, setClicked] = useState(false);
+
+}
