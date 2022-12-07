30bef0bb78208be1be643370e7257ed562f886e9
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-07 08:49:20 +0300

    style: landing page - style landing page with custom logo and html

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :comments
   resources :skill_sets, only: [:index, :show, :update]
+
   post "/signup", to: "users#create"
   get "/me", to: "users#show"
+  
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 
