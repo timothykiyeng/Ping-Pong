f66e2c3e71a221123448b267ea8da343fcf46e14
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-07 14:35:19 +0300

    ft: handleLogOutOnClick - logs out user

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -3,4 +3,12 @@ import { Link } from 'react-router-dom';
 
 function Navbar ( {user, setUser} ) {
 
+    function handleLogoutOnClick() {
+        fetch("/logout", { method: "DELETE" }).then((r) => {
+          if (r.ok) {
+            setUser(null);
+          }
+        });
+      }
+
 }
