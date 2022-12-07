9aaf0f1687131f31d1df838973259b190214da7a
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-07 10:16:40 +0300

    ft: handleSubmit - handle form submission on signup

--- client/src/Components/LoginForm.js
+++ client/src/Components/LoginForm.js
@@ -9,8 +9,10 @@ function LoginForm({ onLogin }) {
     const [isLoading, setIsLoading] = useState(false);
 
     function handleSubmit(e) {
+      
         e.preventDefault();
         setIsLoading(true);
+
         fetch("/login", {
           method: "POST",
           headers: {
--- client/src/Components/SignUpForm.js
+++ client/src/Components/SignUpForm.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 
-function SignUpForm(){
+function SignUpForm( {onLogin} ){
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
@@ -8,5 +8,33 @@ function SignUpForm(){
     const [errors, setErrors] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
 
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        setErrors([]);
+        setIsLoading(true);
+
+        fetch("/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username,
+            password,
+            confirmPassword
+          }),
+        }).then((r) => {
+          setIsLoading(false);
+          if (r.ok) {
+            r.json().then((user) => onLogin(user));
+          } else {
+            r.json().then((err) => setErrors(err.errors));
+          }
+        });
+      }
+
 
 }
+
+export default SignUpForm
