17969adaff990444a0436276e11d00058e418793
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-07 10:52:11 +0300

    ft: Login form - display login and signup form to user

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -6,5 +6,36 @@ function Login( {onLogin} ){
 
     const [showLogin, setShowLogin] = useState(true);
 
+    return (
+        <div className="login-form">
+        <Wrapper>
+          <Logo>Home of Ping Pong</Logo>
+          <h2 className="tagline">Welcome to Home of Ping Pong. Learn to play table tennis and level up your game 🏓 🏓 .</h2>
+          {showLogin ? (
+            <>
+              <LoginForm onLogin={onLogin} />
+              <Divider />
+              <p className="accountquestion">
+                Don't have an account?</p> &nbsp;
+                <Button onClick={() => setShowLogin(false)}>
+                  Sign Up
+                </Button>
+            </>
+          ) : (
+            <>
+              <SignUpForm onLogin={onLogin} />
+              <Divider />
+              <p className="accountquestion">
+                Already have an account?</p> &nbsp;
+                <Button onClick={() => setShowLogin(true)}>
+                  Log In
+                </Button>
+            </>
+          )}
+        </Wrapper>
+        </div>
+      );
+
 
 }
+export default Login
