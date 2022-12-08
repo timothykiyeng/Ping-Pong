f1bf51d45288e37bf24b143d6495b2c336579984
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-08 21:45:00 +0300

    perf: dependecies - install client dependency to package.json

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
@@ -18,14 +18,11 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-  <React.StrictMode>
-    <GlobalStyle/>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
+ReactDOM.render(
+  <Router>
+    <GlobalStyle />
+    <App />
+  </Router>,
+  document.getElementById('root')
 );
 
