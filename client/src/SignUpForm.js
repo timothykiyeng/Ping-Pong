2ed18f60aad80ddb9113294b5564854296c1d0da
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-08 14:53:39 +0300

    ft: component styling - optimized styling for Button

--- client/src/SignUpForm.js
+++ client/src/SignUpForm.js
@@ -109,3 +109,55 @@ const Input = styled.input`
   border-radius: 25px;
   background-color: #edfafd;
 `;
+
+function Button({ variant = "fill", color = "primary", ...props }) {
+  let Component;
+  if (variant === "fill") {
+    Component = FillButton;
+  } else if (variant === "outline") {
+    Component = OutlineButton;
+  }
+
+  return <Component style={COLORS[color]} {...props} />;
+}
+
+const FillButton = styled(ButtonBase)`
+  background-color: white;
+  color: darkblue;
+
+  &:hover {
+    opacity: 0.9;
+  }
+`;
+
+const OutlineButton = styled(ButtonBase)`
+  background-color: white;
+  color: darkblue;
+  border: 2px solid var(--main);
+
+  &:hover {
+    background: hsl(235deg 85% 97%);
+  }
+`;
+const ButtonBase = styled.button`
+  cursor: pointer;
+  font-size: 1rem;
+  border: 1px solid transparent;
+  border-radius: 20px;
+  padding: 8px 16px;
+  margin-left: 40%;
+  margin-right: 40%;
+  margin-bottom: 10%;
+  text-decoration: none;
+`;
+
+const COLORS = {
+  primary: {
+    "--main": "deepskyblue",
+    "--accent": "white",
+  },
+  secondary: {
+    "--main": "white",
+    "--accent": "lightblue",
+  },
+};
