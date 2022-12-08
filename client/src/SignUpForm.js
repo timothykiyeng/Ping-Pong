4f32bdb13e6f092454aabede36d57406e0ccac3f
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-12-08 14:50:48 +0300

    ft: component styling - optimized styling for Input

--- client/src/SignUpForm.js
+++ client/src/SignUpForm.js
@@ -97,3 +97,15 @@ const Label = styled.label`
   margin-bottom: 8px;
 `;
 
+const Input = styled.input`
+  border-color: #dbdbdb;
+  -webkit-appearance: none;
+  max-width: 100%;
+  width: 100%;
+  font-size: 1.2rem;
+  line-height: 1.5;
+  padding: 10px;
+  border: none;
+  border-radius: 25px;
+  background-color: #edfafd;
+`;
